47656f62565dab1a6f3a3db50731fa8ecaa5dfc7
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-20 18:08:19 +0600

    finished making the game

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+while True:
+    try:
+        ask = input('Enter a number: ')
+        print('Press q to quite: ')
+        ask = ask.lower()
+        if ask == "q":
+            break
+        ask = int(ask)
+        if (ask % 3) == 0 and (ask % 5) == 0:
+            print("Fizz Buzz !! :}")
+        elif (ask % 3) == 0:
+            print('Fizz !! :)')
+        elif (ask % 5) == 0:
+            print('Buzz !! :)')
+        else:
+            print(f'{ask} :( \n Try other numbers')
+    except Exception as error:
+        print(f'press only "q" and numbers\nThe error is {error}')
+print('Thanks for playing the game')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+The game is made by Shaidur Rahman
+email: shaidurrahmansaad@gmail.com
+github: github.com/Shajidur-Rahman/
+Thanks for visiting my code.
+"""
